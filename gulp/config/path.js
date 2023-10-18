5aaf155ab9cb198200fa124a1c949398072ed463
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-10-18 23:14:15 +0300

    add start gulp settings

--- gulp/config/path.js
+++ gulp/config/path.js
@@ -1,20 +1,20 @@
-import * as nodePath from 'path';
-const rootFolder = nodePath.basename(nodePath.resolve());
-const buildFolder = `./dist`;
-const srcFolder = `./src`;
+// import * as nodePath from 'path';
+// const rootFolder = nodePath.basename(nodePath.resolve());
+// const buildFolder = `./dist`;
+// const srcFolder = `./src`;
 
-export const path = {
-  build: {
-    files: `${buildFolder}/files/`
-  },
-  src: {
-    files: `${buildFolder}/files/**/*.*`,
-  },
-  watch: {},
-  clean: buildFolder,
-  buildFolder:buildFolder,
-  srcFolder: srcFolder,
-  rootFolder: rootFolder,
-  ftp: ``
-}
+// export const path = {
+//   build: {
+//     files: `${buildFolder}/files/`
+//   },
+//   src: {
+//     files: `${buildFolder}/files/**/*.*`,
+//   },
+//   watch: {},
+//   clean: buildFolder,
+//   buildFolder:buildFolder,
+//   srcFolder: srcFolder,
+//   rootFolder: rootFolder,
+//   ftp: ``
+// }
 
