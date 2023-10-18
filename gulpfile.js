f6fe4c51a822a1e6ff3b599cecada068630c3aa2
Maryna Shavlak <shavlakmaryna@gmail.com>
2023-10-19 00:29:32 +0300

    add styles and index.html

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,8 @@ const include =  require('gulp-include');
 function pages() {
   return src('app/pages/*.html')
   .pipe(include({
-    includePaths: 'app/components'
+    // includePaths: 'app/components'
+    includePaths: 'app/partials'
   }))
   .pipe(dest('app'))
   .pipe(browserSync.stream())
@@ -37,8 +38,19 @@ function fonts() {
 
 }
 
+
+// function concatScss() {
+//   return src(scssFiles)
+//     .pipe(concat('index.scss'))
+//     .pipe(scss().on('error', scss.logError))
+//     .pipe(dest('app/styles'));
+// }
+
+
+
 function styles() {
-  return src('app/scss/style.scss')
+  return src('app/styles/index.scss')
+  // return src(scssFiles )
   .pipe(autoprefixer({overrideBrowsersList:['last 10 version']}))
   .pipe(concat('style.min.css'))
   .pipe(scss({outputStyle: 'compressed'}))
@@ -96,15 +108,13 @@ function watching() {
   browserSync.init({server:{
     baseDir: "app/"
   }})
-  watch(['app/scss/style.scss'], styles)
+  watch(['app/styles/style.scss'], styles)
   watch(['app/images/src'], images)
   watch(['app/js/main.js'], scripts)
-  watch(['app/components/*', 'app/pages/*'], pages)
+  // watch(['app/components/*', 'app/pages/*'], pages)
+  watch(['app/partials/*', 'app/pages/*'], pages)
   watch(['app/**/*.html']).on('change', browserSync.reload) 
 }
-
-
-
 
 function building() {
   return src([
@@ -120,14 +130,55 @@ function building() {
 .pipe(dest('dist'))
 }
 
-
 function cleanDist() {
   return src('dist')
   .pipe(clean())
 }
 
 
+const scssFiles = [
+  'app/styles/utils/common.scss',
+  'app/styles/utils/variables.scss',
+  'app/styles/utils/visually-hidden.scss',
+  'app/styles/base/base.scss',
+  'app/styles/base/container.scss',
+  'app/styles/base/section.scss',
+  'app/styles/components/calendar.scss',
+  'app/styles/components/time-picker.scss',
+  'app/styles/components/buttons.scss',
+  'app/styles/components/home_header.scss',
+  'app/styles/components/hero.scss',
+  'app/styles/components/benefits.scss',
+  'app/styles/components/cleaning-types.scss',
+  'app/styles/components/buildings.scss',
+  'app/styles/components/cleaning-package.scss',
+  'app/styles/components/reviews.scss',
+  'app/styles/components/trust.scss',
+  'app/styles/components/questions.scss',
+  'app/styles/components/connection.scss',
+  'app/styles/components/order-banner.scss',
+  'app/styles/components/support-modal.scss',
+  'app/styles/components/subscription-modal.scss',
+  'app/styles/components/office-calculation.scss',
+  'app/styles/components/success-order-table.scss',
+  'app/styles/components/footer.scss',
+  'app/styles/components/scroll-to-top-btn.scss',
+  'app/styles/components/work-shedule.scss',
+  'app/styles/components/theme-toggler.scss',
+  'app/styles/components/animations.scss',
+  'app/styles/base/modifiers.scss',
+];
 
+
+
+
+
+
+
+
+
+
+// exports.concatScss = concatScss;
 exports.pages= pages;
 exports.styles= styles;
 exports.images= images;
@@ -139,4 +190,5 @@ exports.watching= watching;
 
 
 exports.build = series(cleanDist,building)
-exports.default = parallel(styles, scripts, pages, watching );
+// exports.default = parallel(concatScss, styles, scripts, pages, watching );
+exports.default = parallel( styles, scripts, pages, watching );
